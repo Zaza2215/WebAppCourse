71d0804bcef79eb47ae1cc8fe2e460cea06f4efa
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-11-18 08:44:43 +0100

    Docker & dependencies & linters

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest', sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    settings: {react: {version: '18.2'}},
+    plugins: ['react-refresh', '@typescript-eslint'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        'react/prop-types': 0,
+        'indent': ['error', 4],
+        'linebreak-style': 1,
+        'quotes': ["error", "double"],
+        "semi": ["error", "never"],
+        "react/jsx-first-prop-new-line": ["error", "multiline"],
+        "react/jsx-max-props-per-line": ["error", {"maximum": 1}]
+    },
 }
